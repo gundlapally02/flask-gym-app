29b6d6b94f4ddb604016aa983ae841ecf8a2ee15
skr <skr@sujiths-MacBook-Air.local>
2026-02-22 19:34:29 +0530

    remove wrong app.run causing render 502

--- app.py
+++ app.py
@@ -10,14 +10,15 @@ import os
 
 app = Flask(__name__)
 
-# Ensure correct database path
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_PATH = os.path.join(BASE_DIR, "instance", "gym_recommendations.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_PATH}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'  # Required for session management
+app.config['SECRET_KEY'] = 'your_secret_key'
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 
 # Database Model
 class User(db.Model):
@@ -277,3 +278,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
